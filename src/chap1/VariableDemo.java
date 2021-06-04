8c96180a1085779c7da4f557ad7412c3029b4b40
=samirvithlani <=samir.vithlani83955@gmail.com>
2021-06-04 08:55:25 +0530

    chap1

--- src/chap1/VariableDemo.java
+++ src/chap1/VariableDemo.java
@@ -1,5 +1,7 @@
 package chap1;
 
+import java.util.Scanner;
+
 public class VariableDemo {
 
 	// class level variable declare :-
@@ -13,6 +15,7 @@ public class VariableDemo {
 
 	void add() {
 
+		// withput return type wo args
 		int x = 100; // local variable...
 		System.out.println("add function called.....");
 		System.out.println(x);
@@ -20,12 +23,64 @@ public class VariableDemo {
 
 	}
 
+	// with return type without args
+
+	// data type
+	// 100 int
+	int sum() {
+
+		int a = 10, b = 20;
+		int c = a + b;
+
+		return c;
+		// return a + b; pref
+		// return 100;
+	}
+
+	// with r with args
+	float pers(float maths, float science, float eng) {
+
+		return (maths + science + eng) / 3;
+	}
+
+	double salaryCalulation(int emp1, float emp2, double emp3) {
+
+		return emp1 + emp2 + emp3;
+	}
+
 	public static void main(String[] args) {
 
 		// UDF Object
 		// v1 -> local object
 		VariableDemo v1 = new VariableDemo();
-		v1.add();
+//		v1.add();
+//
+//		// x is loacal variable...
+//		int x = v1.sum();
+//		// what ans return it will print only..
+//		System.out.println(v1.sum());
+//		x = x + 20;
+//		System.out.println("x = " + x);
+
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("enter maths marks");
+//		float m = sc.nextFloat();
+//		System.out.println("enter maths marks");
+//		float s = sc.nextFloat();
+//		System.out.println("enter maths marks");
+//		float e = sc.nextFloat();
+//		
+//		
+//		
+//		float avg = v1.pers(m, s, e);
+//		System.out.println("avg ="+avg);
+//		
+
+		v1.salaryCalulation(100, 100.25f, 25.25);
+		//v1.salaryCalulation(100.20f, 100.25f, 25.25); Error
+		
+		
 
 	}
 
